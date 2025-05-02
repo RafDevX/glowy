291cfd515b3d18dc35cd6a9a6f15ebf84844c516
Rafael Oliveira <rmfseo@kth.se>
2025-05-02 20:12:41 +0200

    refactor: extract Analyzer into separate module

---
+++ src/analyzer.rs
@@ -0,0 +1,189 @@
+use std::{
+    fs,
+    io::{self, BufRead},
+    path,
+};
+
+use crate::SourceFile;
+
+/// Primary orchestrator and conductor of the analysis process.
+///
+/// This is main entrypoint into Glowy's logic and encapsulates all
+/// implementation complexity. Library users should construct an instance using
+/// [`Analyzer::new`] or [`Analyzer::from_go_mod`], configure any relevant
+/// options (including adding source files with [`Analyzer::add_file`]), and
+/// then execute the analysis with [`Analyzer::analyze`].
+///
+/// # Example Usage
+///
+/// ```
+/// let analyzer = glowy::Analyzer::from_directory("./proj")?.expect("module path");
+///
+/// let result = analyzer.analyze();
+/// ```
+pub struct Analyzer {
+    module_base: String,
+    files: Vec<SourceFile>,
+}
+
+impl Analyzer {
+    /// Constructs a new bare instance of [`Analyzer`].
+    ///
+    /// The `module_base` argument is the module path of the Go module that will
+    /// be analyzed, such as `example.com/company-name/proj`. Any inner packages
+    /// within the module will be associated with paths relative to this value,
+    /// allowing for imports like `import "example.com/company-name/proj/auth"`
+    /// to be resolved.
+    ///
+    /// # Example Usage
+    ///
+    /// ```
+    /// let mut analyzer = glowy::Analyzer::new("example.com/company-name/proj");
+    /// ```
+    ///
+    /// # See Also
+    ///
+    /// It's often more convenient to instead use the
+    /// [`Analyzer::from_directory`] utility or [`Analyzer::from_go_mod`], which
+    /// are helpful wrappers around this method.
+    pub fn new(module_base: &str) -> Self {
+        Self {
+            module_base: module_base.to_owned(),
+            files: Vec::new(),
+        }
+    }
+
+    /// Constructs a new instance of [`Analyzer`] from a Go module directory.
+    ///
+    /// This is the recommended constructor for most situations, where all
+    /// Go source code files should be read from a unified directory on disk,
+    /// the root of which contains a `go.mod` file that specifies the base
+    /// module path (via a `module` directive).
+    ///
+    /// Internally, this method uses [`Analyzer::from_go_mod`] and
+    /// [`SourceFile::read_from_disk`], so their respective conditions apply.
+    /// In particular, this method returns `Ok(None)` if no valid `module`
+    /// directive was found in the `go.mod` file.
+    ///
+    /// # Errors
+    ///
+    /// An [`std::io::Error`] is returned if any filesystem operation fails,
+    /// including (but not limited to):
+    ///     - if the specified path does not correspond to an (accessible)
+    ///       directory;
+    ///     - if no `go.mod` file exists or could be opened;
+    ///     - if a file with `.go` extension could not be read or contains
+    ///       invalid UTF-8 sequences.
+    ///
+    /// # Example Usage
+    ///
+    /// ```
+    /// let analyzer = glowy::Analyzer::from_directory("./proj")?.expect("module path");
+    /// ```
+    pub fn from_directory<P: AsRef<path::Path>>(path: P) -> io::Result<Option<Self>> {
+        let mut analyzer = match Self::from_go_mod(path.as_ref().join("go.mod"))? {
+            Some(analyzer) => analyzer,
+            None => return Ok(None),
+        };
+
+        analyzer.add_directory_recurs(path::Component::RootDir, path)?;
+
+        Ok(Some(analyzer))
+    }
+
+    /// Constructs a new instance of [`Analyzer`] based on a `go.mod` file.
+    ///
+    /// This method is a wrapper around [`Analyzer::new`] that provides the
+    /// convenience of extracting the base Go module path directly from a
+    /// specified `go.mod` file. The file residing at the given path is opened
+    /// in read-only mode and the module path is extracted from the first
+    /// `module` directive per the [spec](https://go.dev/ref/mod).
+    ///
+    /// If no valid `module` directive is found, this method returns `Ok(None)`.
+    ///
+    /// # Errors
+    ///
+    /// Any [`std::io::Error`] resulting from opening the file (such as it not
+    /// existing) is returned as-is. The file is only read while no errors
+    /// occur; if there is a read failure, no further attempts are performed and
+    /// the method returns `Ok(None)` since no valid `module` directive was
+    /// found.
+    ///
+    /// # Example Usage
+    ///
+    /// ```
+    /// let mut analyzer = glowy::Analyzer::from_go_mod("./proj/go.mod")?;
+    /// ```
+    pub fn from_go_mod<P: AsRef<path::Path>>(path: P) -> io::Result<Option<Self>> {
+        let file = fs::File::open(path)?;
+        let reader = io::BufReader::new(file);
+        let lines = reader.lines();
+
+        for line in lines.map_while(Result::ok) {
+            if let Some(base) = line.trim().strip_prefix("module ") {
+                let base = base.split("//").next().unwrap().trim();
+
+                // TODO: support alternative syntax per spec
+                // "(" newline ModulePath newline ")"
+
+                if !base.is_empty() {
+                    return Ok(Some(Self::new(base)));
+                }
+            }
+        }
+
+        Ok(None)
+    }
+
+    fn add_directory_recurs<V: AsRef<path::Path>, R: AsRef<path::Path>>(
+        &mut self,
+        virtual_path: V,
+        real_path: R,
+    ) -> io::Result<()> {
+        for entry in fs::read_dir(real_path)? {
+            let entry = entry?;
+            let file_type = entry.file_type()?;
+
+            if file_type.is_dir() {
+                self.add_directory_recurs(
+                    virtual_path.as_ref().join(entry.file_name()),
+                    entry.path(),
+                )?;
+            } else if file_type.is_file() {
+                let real_path = entry.path();
+
+                if real_path.extension().filter(|e| *e == "go").is_none() {
+                    continue;
+                }
+
+                let file = SourceFile::read_from_disk(
+                    virtual_path.as_ref().join(entry.file_name()),
+                    real_path,
+                )?;
+
+                self.add_file(file);
+            }
+            // else: file is a symlink; ignore (unsupported)
+        }
+
+        Ok(())
+    }
+
+    /// Adds a new file to be analyzed.
+    ///
+    /// See [`SourceFile`] for more information on how to construct one.
+    ///
+    /// # Example Usage
+    ///
+    /// ```
+    /// let file = glowy::SourceFile::read_from_disk("/main.go", "./proj/main.go")?;
+    /// analyzer.add_file(file);
+    /// ```
+    pub fn add_file(&mut self, file: SourceFile) {
+        self.files.push(file);
+    }
+
+    pub fn analyze(&self) {
+        todo!()
+    }
+}
