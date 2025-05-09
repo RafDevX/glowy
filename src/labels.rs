beef475e929c53b35909652e445386ef572fa61f
Rafael Oliveira <rmfseo@kth.se>
2025-05-09 23:05:57 +0200

    feat: add LabelBacktrace

--- src/labels.rs
+++ src/labels.rs
@@ -9,8 +9,13 @@
 //! Labels are essentially just sets of **tags**, such as `{admiral, secret}`,
 //! which may be empty (`{ }`, also called **bottom**). For more information,
 //! see [`Label`] and [`LabelTag`].
+//!
+//! These labels' evolution and propagation history can be easily tracked using
+//! a hierarchy structure, which is here implemented via [`LabelBacktrace`].
 
 use std::{cmp, collections::BTreeSet, fmt};
+
+use crate::ScopedSpan;
 
 /// Represents an individual tag within a label.
 ///
@@ -48,6 +53,8 @@ impl<'a> fmt::Display for LabelTag<'a> {
 /// used instead of an empty set (i.e., `{ }`). Formally, a `Top` variant could
 /// also exist, but it is not actually needed in Glowy so it is not here
 /// implemented.
+///
+/// Label derivation and hierarchy is tracked through [`LabelBacktrace`].
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Label<'a> {
     /// A non-empty set of [`LabelTag`]s.
@@ -68,6 +75,45 @@ impl<'a> Label<'a> {
             let set = BTreeSet::from_iter(tags.iter().map(|tag| LabelTag::Concrete(tag)));
 
             Label::Tags(set)
+        }
+    }
+
+    /// Returns the intersection of `self` and `other` as a new [`Label`].
+    ///
+    /// For example, `{a, b, c}` intersected with `{b, d, e}` yields `{b}`.
+    /// If the intersection is null, [`Label::Bottom`] is returned as expected.
+    pub fn intersect(&self, other: &Self) -> Self {
+        match (self, other) {
+            (Self::Bottom, _) | (_, Self::Bottom) => Self::Bottom,
+            (Self::Tags(left), Self::Tags(right)) => {
+                let new_tags = left & right;
+
+                if new_tags.is_empty() {
+                    Self::Bottom
+                } else {
+                    Self::Tags(new_tags)
+                }
+            }
+        }
+    }
+
+    /// Returns the difference of `self` and `other` as a new [`Label`].
+    ///
+    /// For example, `{a, c}` subtracted from `{a, b, c}` yields `{b}`.
+    /// If the difference is null, [`Label::Bottom`] is returned as expected.
+    pub fn difference(&self, other: &Self) -> Self {
+        match (self, other) {
+            (Self::Bottom, _) => Self::Bottom,
+            (_, Self::Bottom) => self.clone(),
+            (Self::Tags(left), Self::Tags(right)) => {
+                let new_tags = left - right;
+
+                if new_tags.is_empty() {
+                    Self::Bottom
+                } else {
+                    Self::Tags(new_tags)
+                }
+            }
         }
     }
 }
@@ -113,4 +159,139 @@ impl<'a> fmt::Display for Label<'a> {
             Label::Bottom => write!(f, "{{}}"),
         }
     }
+}
+
+/// Represents the propagation history leading up to a label attribution.
+///
+/// This hierarchical structure keeps track of why a certain piece of data has
+/// been assigned a specific [`Label`], particularly in the sense of what
+/// operations occurred and compounded to result in a final label.
+///
+/// The following invariants are strictly enforced:
+///     1. No [`LabelBacktrace`] ever has a label [`Label::Bottom`];
+///     2. Children's labels are always a subset of their parent's label (e.g.,
+///     a backtrace `{blue, violet}` can have a child `{blue}` and another
+///     `{violet}`, but never a `{yellow}` child); and
+///     3. All children's labels are always disjoint (e.g., if a first child
+///     has label `{blue, violet}`, a second child can never have label
+///     `{blue, yellow}` -- it will instead be trimmed to just `{yellow}` to
+///     simplify the whole chain and limit hierarchy size).
+#[derive(Debug, Clone, PartialEq)]
+pub struct LabelBacktrace<'a> {
+    /// What operation caused this label attribution.
+    kind: LabelBacktraceKind,
+    /// The final label associated with some information.
+    label: Label<'a>,
+    /// Name of symbol with this label, if any/applicable.
+    symbol: Option<&'a str>,
+    /// Location where this operation took place.
+    place: ScopedSpan<'a>,
+    /// Other backtraces through which this label is derived via propagation.
+    children: Vec<LabelBacktrace<'a>>,
+}
+
+impl<'a> LabelBacktrace<'a> {
+    /// Base case; no children
+    pub(crate) fn new_explicit_annotation(
+        label: Label<'a>,
+        symbol: &'a str,
+        place: ScopedSpan<'a>,
+    ) -> Self {
+        Self {
+            kind: LabelBacktraceKind::ExplicitAnnotation,
+            label,
+            symbol: Some(symbol),
+            place,
+            children: vec![],
+        }
+    }
+
+    /// Returns [`None`] iff `label` is [`Label::Bottom`].
+    pub(crate) fn new(
+        kind: LabelBacktraceKind,
+        label: Label<'a>,
+        symbol: Option<&'a str>,
+        place: ScopedSpan<'a>,
+        children: &[&Self],
+    ) -> Option<Self> {
+        if label == Label::Bottom {
+            return None;
+        }
+
+        let mut remaining_label = label.clone();
+        let children: Vec<_> = children
+            .iter()
+            .filter_map(|child| {
+                child
+                    .restrict_to_label(&remaining_label)
+                    .inspect(|child| remaining_label = remaining_label.difference(&child.label))
+            })
+            .collect();
+
+        Some(Self {
+            kind,
+            label,
+            symbol,
+            place,
+            children,
+        })
+    }
+
+    /// Returns a new instance whose label only contains tags in a given
+    /// constraint, pruning children if they would have [`Label::Bottom`].
+    fn restrict_to_label(&self, constraint: &Label<'a>) -> Option<Self> {
+        let new_label = self.label.intersect(constraint);
+
+        if new_label == Label::Bottom {
+            return None;
+        }
+
+        Some(Self {
+            kind: self.kind,
+            label: new_label,
+            symbol: self.symbol,
+            place: self.place.clone(),
+            children: self
+                .children
+                .iter()
+                .filter_map(|child| child.restrict_to_label(constraint))
+                .collect(),
+        })
+    }
+
+    /// Returns the kind of operation that caused the label assignment.
+    pub fn kind(&self) -> &LabelBacktraceKind {
+        &self.kind
+    }
+
+    /// Returns the label in question described by this backtrace.
+    pub fn label(&self) -> &Label<'a> {
+        &self.label
+    }
+
+    /// Returns the name of the symbol with this label, if any/applicable.
+    pub fn symbol(&self) -> Option<&'a str> {
+        self.symbol
+    }
+
+    /// Returns the location where the operation took place.
+    pub fn place(&self) -> &ScopedSpan<'a> {
+        &self.place
+    }
+
+    /// Returns the backtraces that compound to yield and justify this one.
+    pub fn children(&self) -> &[Self] {
+        &self.children
+    }
+}
+
+/// The concrete operation that resulted in a label assignment.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum LabelBacktraceKind {
+    /// Explicit source code annotation.
+    ExplicitAnnotation,
+    /// Assignment of some tainted expression to a variable.
+    Assignment,
+    /// Compounded label derived from the parts of a composite expression.
+    Expression,
 }
