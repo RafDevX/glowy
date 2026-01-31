5ca1e3bdd9160430e71dc27ac28012f2615890ac
Rafael Oliveira <rmfseo@kth.se>
2026-01-31 19:04:27 +0100

    refactor(taint): extract some expression visitors
    This simply moves some functions into separate modules for easier
    organization and reduce file length for the main `exprs.rs`.

---
+++ src/taint/exprs/component.rs
@@ -0,0 +1,149 @@
+use parser::ast::{IndexingNode, SelectionNode, SlicingNode};
+
+use crate::{
+    context::AnalysisContext,
+    errors::AnalysisErrorKind,
+    labels::LabelBacktraceKind,
+    values::{
+        BacktraceContainer, ExpandableValue, SelfAwareBacktraceContainer, SimpleConstValue, Value,
+        ValueRef,
+    },
+};
+
+pub fn visit_selection<'a>(
+    ctx: &mut AnalysisContext<'a>,
+    node: &SelectionNode<'a>,
+) -> ValueRef<'a> {
+    let base = super::visit_single_expr(ctx, &node.base);
+
+    if let Some(pkg) = base.as_package_ref() {
+        // this is not actually a selection, it's just a qualified operand name
+
+        return super::visit_operand_name(ctx, node.selector, Some(pkg.qualifier()));
+    }
+
+    let Some(r#struct) = base.as_struct() else {
+        ctx.report_error(AnalysisErrorKind::InvalidSelectionBase {
+            location: node.location.clone(),
+        });
+
+        return ValueRef::new_bottom();
+    };
+
+    r#struct.get_const(
+        node.selector.content().to_owned(),
+        ctx.pin(node.location.clone()),
+    )
+}
+
+pub fn visit_indexing<'a>(ctx: &mut AnalysisContext<'a>, node: &IndexingNode<'a>) -> ValueRef<'a> {
+    let base = super::visit_single_expr(ctx, &node.base);
+
+    let Some(composite) = base.as_composite() else {
+        ctx.report_error(AnalysisErrorKind::InvalidIndexingBase {
+            location: node.location.clone(),
+        });
+
+        return ValueRef::new_bottom();
+    };
+
+    let index = SimpleConstValue::try_resolve_from_expr(&node.index);
+
+    let result = if let Some(index) = index {
+        composite.get_const(index, ctx.pin(node.location.clone()))
+    } else {
+        composite.get_dyn(ctx.pin(node.location.clone()))
+    };
+
+    if base.is_map() {
+        // indexing a map returns a second value corresponding to whether the
+        // key was or not present in the map. here, we assume that this presence
+        // value has the same label as the actual returned value
+        let presence = result.backtrace_at_location(ctx.pin(node.location.clone()));
+
+        ValueRef::from(Value::Expandable(ExpandableValue::new(
+            result,
+            vec![ValueRef::from(presence)],
+        )))
+    } else {
+        result
+    }
+}
+
+pub fn visit_slicing<'a>(ctx: &mut AnalysisContext<'a>, node: &SlicingNode<'a>) -> ValueRef<'a> {
+    let base = super::visit_single_expr(ctx, &node.base);
+
+    let location = ctx.pin(node.location.clone());
+
+    // per spec, string slicing is only allowed if max is None
+    // (full slicing expressions only support arrays/slices)
+    let result = if node.max.is_none() && base.is_simple() {
+        // either we're slicing a simple string (creating a substring), or base
+        // actually has a more complex shape but just hasn't been coerced yet
+        // (in which case its final "dyn + all consts" backtrace would just be
+        // the current simple value) -- in both cases, the result of accessing
+        // it is always just the backtrace itself (+ low/high/max)
+
+        base.backtrace_at_location(ctx.pin(super::get_expr_location(&node.base)))
+    } else if let Some(sliceable) = base.as_complex_sliceable() {
+        let low = node
+            .low
+            .as_deref()
+            .map(SimpleConstValue::try_resolve_from_expr);
+        let high = node
+            .high
+            .as_deref()
+            .map(SimpleConstValue::try_resolve_from_expr);
+
+        // at this point low and high are both Option<Option<SimpleConstValue>>,
+        // but we actually need to match on the inner Option (representing
+        // whether a const value was determined) to know whether to use const
+        // or dyn slicing, and the outer Option (representing whether a concrete
+        // low/high value was explicitly provided or just omitted) should just
+        // be propagated. this means we need to do some rather unintuitive
+        // matching here to essentially swap the Options
+        #[inline]
+        fn transform(v: Option<Option<SimpleConstValue>>) -> Option<Option<u64>> {
+            match v {
+                Some(Some(SimpleConstValue::Integer(x))) => Some(Some(x)),
+                Some(_) => None,
+                None => Some(None),
+            }
+        }
+
+        let low = transform(low);
+        let high = transform(high);
+
+        match (low, high) {
+            (Some(low), Some(high)) => sliceable.slice_const(low, high, location.clone()),
+            _ => sliceable.slice_dyn(location.clone()),
+        }
+    } else {
+        ctx.report_error(AnalysisErrorKind::InvalidSlicingBase {
+            location: node.location.clone(),
+        });
+
+        None
+    };
+
+    ValueRef::from(
+        result.nest_backtrace(
+            LabelBacktraceKind::Expression,
+            None,
+            location,
+            [
+                node.low
+                    .as_ref()
+                    .and_then(|l| super::get_expr_backtrace(ctx, l)),
+                node.high
+                    .as_ref()
+                    .and_then(|h| super::get_expr_backtrace(ctx, h)),
+                node.max
+                    .as_ref()
+                    .and_then(|m| super::get_expr_backtrace(ctx, m)),
+            ]
+            .into_iter()
+            .flatten(),
+        ),
+    )
+}
---
+++ src/taint/exprs/literals.rs
@@ -0,0 +1,267 @@
+use std::collections::HashMap;
+
+use parser::{
+    Location,
+    ast::{
+        CompositeLiteralElementListNode, CompositeLiteralElementNode, LiteralNode,
+        StructLiteralFieldsNode, TypeNode,
+    },
+};
+
+use crate::{
+    context::AnalysisContext,
+    errors::AnalysisErrorKind,
+    labels::{LabelBacktrace, LabelBacktraceKind},
+    taint::funcs,
+    values::{BacktraceContainer, CompositeValue, SimpleConstValue, Value, ValueRef},
+};
+
+pub fn visit_literal<'a>(ctx: &mut AnalysisContext<'a>, node: &LiteralNode<'a>) -> ValueRef<'a> {
+    match node {
+        LiteralNode::Int { .. }
+        | LiteralNode::Float { .. }
+        | LiteralNode::Rune { .. }
+        | LiteralNode::String { .. } => ValueRef::new_bottom(),
+        LiteralNode::Function {
+            signature,
+            body,
+            location,
+        } => funcs::visit_function_literal(ctx, signature, body, location),
+        LiteralNode::Array {
+            values, location, ..
+        } => ValueRef::from(Value::Array(visit_integer_keyed_composite_literal(
+            ctx, values, location,
+        ))),
+        LiteralNode::Slice {
+            values, location, ..
+        } => {
+            // Array length must be a constant so we don't need to visit it to
+            // trigger side-effects (there aren't any); we can focus on values
+            ValueRef::from(Value::Slice(visit_integer_keyed_composite_literal(
+                ctx, values, location,
+            )))
+        }
+        LiteralNode::Map {
+            values, location, ..
+        } => ValueRef::from(Value::Map(visit_map_composite_literal(
+            ctx, values, location,
+        ))),
+        LiteralNode::Struct {
+            r#type,
+            fields,
+            location,
+            ..
+        } => ValueRef::from(Value::Struct(visit_struct_composite_literal(
+            ctx, fields, r#type, location,
+        ))),
+    }
+}
+
+fn visit_integer_keyed_composite_literal<'a>(
+    ctx: &mut AnalysisContext<'a>,
+    values: &CompositeLiteralElementListNode<'a>,
+    location: &Location,
+) -> CompositeValue<'a, u64> {
+    let mut map = HashMap::new();
+    let mut others = Vec::new();
+
+    let mut next_default_key = 0;
+    for (opt_key, el) in values {
+        let value = visit_array_literal_element(ctx, el, location);
+
+        if value.is_bottom() {
+            // we don't need to bloat the HashMap with None backtraces
+            next_default_key += 1;
+
+            continue;
+        }
+
+        let key = if let Some(expr) = opt_key {
+            if let Some(SimpleConstValue::Integer(int)) =
+                SimpleConstValue::try_resolve_from_expr(expr)
+            {
+                Some(int)
+            } else {
+                // should not happen for arrays/slices, but you never know
+                // (more complex const expressions won't be resolved)
+                None
+            }
+        } else {
+            Some(next_default_key)
+        };
+
+        if let Some(key) = key {
+            next_default_key = key + 1;
+
+            map.insert(key, value);
+        } else {
+            next_default_key += 1; // no proper answer on what to do here...
+
+            others.push(value);
+        }
+    }
+
+    CompositeValue::new(map, others, ctx.pin(location.clone()))
+}
+
+fn visit_map_composite_literal<'a>(
+    ctx: &mut AnalysisContext<'a>,
+    values: &CompositeLiteralElementListNode<'a>,
+    location: &Location,
+) -> CompositeValue<'a, SimpleConstValue> {
+    let mut map = HashMap::new();
+    let mut others = Vec::new();
+
+    for (opt_key, el) in values {
+        let value = visit_array_literal_element(ctx, el, location);
+
+        if value.is_bottom() {
+            // we don't need to bloat the HashMap with None backtraces
+            continue;
+        }
+
+        let const_key = opt_key
+            .as_ref()
+            .and_then(SimpleConstValue::try_resolve_from_expr);
+
+        if let Some(const_key) = const_key {
+            map.insert(const_key, value);
+        } else {
+            if let Some(dyn_key) = opt_key {
+                others.push(super::visit_single_expr(ctx, dyn_key));
+            }
+
+            others.push(value);
+        }
+    }
+
+    CompositeValue::new(map, others, ctx.pin(location.clone()))
+}
+
+fn visit_struct_composite_literal<'a>(
+    ctx: &mut AnalysisContext<'a>,
+    fields: &StructLiteralFieldsNode<'a>,
+    r#type: &TypeNode<'a>,
+    location: &Location,
+) -> CompositeValue<'a, String> {
+    let mut map = HashMap::new();
+    let mut others = Vec::new();
+
+    match fields {
+        StructLiteralFieldsNode::Keyed(entries) => {
+            for (field_name, element) in entries {
+                let value = visit_array_literal_element(ctx, element, location);
+
+                if value.is_bottom() {
+                    // we don't need to bloat the HashMap with None backtraces
+                    continue;
+                }
+
+                if map.insert(field_name.content().to_owned(), value).is_some() {
+                    // duplicate; error
+                    ctx.report_error(AnalysisErrorKind::DuplicateStructFieldName {
+                        duplicate: *field_name,
+                    });
+                }
+            }
+        }
+        StructLiteralFieldsNode::Exhaustive(entries) => {
+            // we try to extract field names from the type information, but in
+            // most cases this will not be possible, and in those cases we can
+            // only pass the field information as "others" (which will become
+            // the basis for the dyn backtrace of the composite value)
+
+            let names = if let TypeNode::Struct { fields } = r#type {
+                let candidate: Vec<_> = fields
+                    .iter()
+                    .flat_map(|f| f.ids.iter())
+                    .map(Option::as_ref)
+                    .collect();
+
+                if candidate.len() == entries.len() {
+                    Some(candidate)
+                } else {
+                    // this should never happen, but oh well
+                    None
+                }
+            } else {
+                None
+            };
+
+            if let Some(names) = names {
+                for (name, element) in names.iter().copied().zip(entries) {
+                    let value = visit_array_literal_element(ctx, element, location);
+
+                    if let Some(&name) = name {
+                        // happy path: we know the field name!
+                        if map.insert(name.content().to_owned(), value).is_some() {
+                            // duplicate; error
+                            // (should never happen, but we don't validate types)
+                            ctx.report_error(AnalysisErrorKind::DuplicateStructFieldName {
+                                duplicate: name,
+                            });
+                        }
+                    } else {
+                        // padding (blank "_" identifier); never accessible so
+                        // we don't need to care about it besides visiting to
+                        // trigger side effects (which we have already done
+                        // above) -- padding fields are always the zero-value
+                        // and are never initialized even if an expression is
+                        // provided: try running this in the Go playground
+                        // ```go
+                        // x := struct{ x, _, y int }{4, 3, -1}
+                        // fmt.Println(x) // prints {4 0 -1}
+                        // ```
+                    }
+                }
+            } else {
+                // nothing to do, we cannot know field names, so we just
+                // approximate by merging all the provided values together
+                for element in entries {
+                    let value = visit_array_literal_element(ctx, element, location);
+
+                    others.push(value);
+                }
+            }
+        }
+    }
+
+    CompositeValue::new(map, others, ctx.pin(location.clone()))
+}
+
+fn visit_array_literal_element<'a>(
+    ctx: &mut AnalysisContext<'a>,
+    node: &CompositeLiteralElementNode<'a>,
+    location: &Location,
+) -> ValueRef<'a> {
+    match &node {
+        CompositeLiteralElementNode::Expr(expr) => super::visit_single_expr(ctx, expr),
+        CompositeLiteralElementNode::Nested(items) => {
+            let mut values: Vec<_> = items
+                .iter()
+                .map(|(_, v)| v)
+                .map(|el| visit_array_literal_element(ctx, el, location))
+                .filter(|v| !v.is_bottom())
+                .collect();
+
+            if values.is_empty() {
+                // quicker escape to avoid clones et al. if they're unnecessary
+                ValueRef::new_bottom()
+            } else if values.len() == 1 {
+                values.pop().unwrap()
+            } else {
+                let backtraces: Vec<_> = values
+                    .iter()
+                    .filter_map(|v| v.backtrace_at_location(ctx.pin(location.clone())))
+                    .collect();
+
+                ValueRef::from(LabelBacktrace::fold(
+                    &backtraces,
+                    LabelBacktraceKind::Expression,
+                    None,
+                    ctx.pin(location.clone()),
+                ))
+            }
+        }
+    }
+}
